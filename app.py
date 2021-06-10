e15150859e59510b3be129aa090bc85365da1a34
lina_d_inrahd <anildharni370z@gmail.com>
2021-06-10 09:39:27 +0530

    changed sizes

--- app.py
+++ app.py
@@ -913,5 +913,5 @@ def update_graph(rec, lst):
 
 
 if __name__ == '__main__':
-    # app.run_server(debug=False)
-    app.run_server(debug=True, port=8049)
+    app.run_server(debug=False)
+    # app.run_server(debug=True, port=8049)
